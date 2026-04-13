17fd9d0dfc1790d7797120bdfb8e0abcfd5c2826
Enrique Ugarte <enriqueugartedem@hotmail.com>
2026-04-12 21:16:27 -0600

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,10 @@
+URL_SERVICE = "https://cnt-4086a6ae-e71d-49ea-a3b3-17fd2cdcac76.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
+
+#documentos
+#documentacion = URL_SERVICE + DOC_PATH
+#print(documentacion)
